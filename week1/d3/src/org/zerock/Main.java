7cea549f986fbde976e7073ba3449e72e5cda4b9
GormWH <sp4kid@gmail.com>
2022-02-16 23:27:38 +0900

    directory change, day3 study log

--- week1/d3/src/org/zerock/Main.java
+++ week1/d3/src/org/zerock/Main.java
@@ -3,6 +3,22 @@ package org.zerock;
 public class Main {
 
     public static void main(String[] args) {
+        Person suhong = new Person();
+        suhong.height = 1.7;
+        suhong.weight = 63;
+        System.out.println(suhong.calcBMI());
 
+    }
+}
+
+class Person {
+
+    String name;
+    String sex;
+    double height;
+    double weight;
+
+    public double calcBMI() {
+        return weight / (height * height);
     }
 }
